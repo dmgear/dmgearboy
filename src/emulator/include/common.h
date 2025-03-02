3f895657e7819195adf546b434b2cbba73d623da
Dylan Gearhart <dylan.gearhart@wmich.edu>
2025-03-02 00:27:32 -0500

    added flags register and comments

--- src/emulator/include/common.h
+++ src/emulator/include/common.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// aliases for unsigned ints
 typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
--- src/emulator/include/cpu.h
+++ src/emulator/include/cpu.h
@@ -2,7 +2,25 @@
 #include <common.h>
 
 typedef struct {
-    u8 A;
+   /*
+    This structure represents the registers of the Game Boy CPU.
+
+    - A: The accumulator register used in arithmetic and logic operations.
+    - F: The flags register, holding the status flags for the CPU (e.g., Zero, Subtraction, Half-carry, Carry).
+    - B: A general-purpose 8-bit register.
+    - C: A general-purpose 8-bit register.
+    - D: A general-purpose 8-bit register.
+    - E: A general-purpose 8-bit register.
+    - H: A general-purpose 8-bit register.
+    - L: A general-purpose 8-bit register.
+    - SP: The Stack Pointer, used for managing function calls and local data storage.
+    - PC: The Program Counter, which holds the address of the next instruction to execute.
+
+    The structure simulates the state of the CPU during the execution of instructions in the Game Boy emulator.
+    */
+
+    u8 A; 
+    u8 F; 
     u8 B;
     u8 C;
     u8 D;
